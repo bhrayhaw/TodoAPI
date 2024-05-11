8eee75fcde82bbfc2e219720e80146c416b502c1
Elijah Apreko <elijahapreko@gmail.com>
2024-05-11 20:55:06 +0000

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
 
+const userschema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userschema);
+
+module.exports = User;
