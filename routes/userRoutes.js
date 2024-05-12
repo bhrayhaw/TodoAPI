4ce0be1ba40f835e488eecdbd6f9214b3184e812
Elijah Apreko <elijahapreko@gmail.com>
2024-05-12 14:41:36 +0000

    added post route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,41 @@
-const express = require("express")
-const User = require("../models/User")
-const router = express.Router()
-
+const express = require("express");
+const User = require("../models/User");
+const validateUser = require("../validations/userValidation");
+const router = express.Router();
 
 // Get list of users
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find()
-        res.status(200).json({"message": "Fetched users successfully", "users": users})
-    } catch (error) {
-        res.status(500).json({"message": error.message[0]})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res
+      .status(200)
+      .json({ message: "Fetched users successfully", users: users });
+  } catch (error) {
+    res.status(500).json({ message: error.message[0] });
+  }
+});
+
+// Add a new user
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(404).json({ error: error.details[0].message });
+
+      const existinguser = await User.findOne({ "email": req.body.email });
+    if (existinguser)
+      return res.status(404).json({ message: "User already exist" });
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
