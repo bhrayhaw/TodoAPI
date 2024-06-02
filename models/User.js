9cf0912ad9faa4e3be2407d20a0198cd4a3a4118
Elijah Apreko <elijahapreko@gmail.com>
2024-06-02 22:13:23 +0000

    update jwt payload

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const userschema = new mongoose.Schema({
 });
 
 userschema.methods.getAuthToken = function () {
-  const token = jwt.sign({ email: this.email }, config.get("jwtSecret"), {
+  const token = jwt.sign({_id: this._id, email: this.email }, config.get("jwtSecret"), {
     expiresIn: "1h",
   });
   return token;
