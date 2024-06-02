0eca7e896fb391f9dca18adb11254a78c0970cb0
Elijah Apreko <elijahapreko@gmail.com>
2024-06-02 22:16:32 +0000

    update routes to include todo

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,11 @@
-const express = require("express")
-const users = require("../routes/userRoutes")
-const auth = require("../routes/authRoutes")
-
+const express = require("express");
+const users = require("../routes/userRoutes");
+const auth = require("../routes/authRoutes");
+const todo = require("../routes/todoRoutes");
 
 module.exports = function (app) {
-    app.use(express.json())
-    app.use("/api/users", users)
-    app.use("/api/", auth)
-}
+  app.use(express.json());
+  app.use("/api/", auth);
+  app.use("/api/users", users);
+  app.use("/api/todo", todo);
+};
