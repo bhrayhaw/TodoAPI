355375ea71367eb8068a7aee959ff63b197d9c68
Elijah Apreko <elijahapreko@gmail.com>
2024-06-02 22:12:33 +0000

    Add todo model

---
+++ models/Todo.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
