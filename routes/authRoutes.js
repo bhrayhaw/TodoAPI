c6474583e646dced98bb28248cd8b0da5a59c7dd
Elijah Apreko <elijahapreko@gmail.com>
2024-06-02 22:13:56 +0000

    update authentication routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -83,18 +83,13 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).json({ message: "Invalid Email" });
 
-    const validPassword = await verifypassword(
-      req.body.password,
-      user.password
-    );
+    const validPassword = await verifypassword(req.body.password, user.password);
     if (!validPassword)
       return res.status(404).json({ message: "Invalid Password" });
 
     const token = user.getAuthToken();
 
-    res.header("x-auth-token").send(token);
-
-    res.status(200).json({ message: "User logged in successfully" });
+    res.status(200).header("x-auth-token", token).json({ message: "User logged in successfully", token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
