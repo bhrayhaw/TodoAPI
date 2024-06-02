f3e8b7d04866499d0839f1d6d905e6309c5c5c06
Elijah Apreko <elijahapreko@gmail.com>
2024-06-02 22:16:11 +0000

    add scheduler config to the dbconfig

--- startup/dbconfig.js
+++ startup/dbconfig.js
@@ -1,10 +1,12 @@
 const config = require("config");
 const mongoose = require("mongoose");
+const Scheduler = require('../services/scheduler')
 
 module.exports = async function () {
   try {
     await mongoose.connect(config.get("uri"));
     console.log("Connected to mongodb");
+    Scheduler()
   } catch (error) {
     console.log(error.message[0]);
   }
